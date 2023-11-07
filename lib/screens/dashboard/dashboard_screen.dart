f7e42a7cbe67f8a5876b6b2267e2209c223fc58f
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-11-07 15:35:09 +1100

    Experimenting with a default dashboard from github

---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,92 @@
+import 'package:device_homepage/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text("Dashboard",
+                    style: Theme.of(context).textTheme.titleLarge),
+                const Spacer(flex: 1),
+                const Expanded(child: SearchField()),
+                const ProfileCard(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: defaultPadding),
+      padding: const EdgeInsets.symmetric(
+        horizontal: defaultPadding,
+        vertical: defaultPadding / 2,
+      ),
+      decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          border: Border.all(color: Colors.white10)),
+      child: Row(
+        children: [
+          Image.asset("assets/images/profile_pic.png", height: 38),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+            child: Text("Admin"),
+          ),
+          const Icon(Icons.keyboard_arrow_down)
+        ],
+      ),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        fillColor: secondaryColor,
+        filled: true,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        suffixIcon: InkWell(
+          onTap: () {},
+          child: Container(
+              padding: const EdgeInsets.all(defaultPadding * 0.75),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+              decoration: const BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: SvgPicture.asset("assets/icons/Search.svg")),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,23 @@
+import 'package:device_homepage/screens/dashboard/dashboard_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:device_homepage/screens/main/components/side_menu.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: SideMenu(),
+          ),
+          Expanded(flex: 5, child: Dashboard()),
+        ],
+      )),
+    );
+  }
+}
